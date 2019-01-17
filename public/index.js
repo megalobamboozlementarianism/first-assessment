d05bf04d191dd942bedafe7d21a8f6460418ad01
megalobamboozlementarianism <abe.coley@gmail.com>
2019-01-17 13:19:43 -0700

    get JS timer up and running

--- public/index.js
+++ public/index.js
@@ -1,4 +1,14 @@
 // build a function that returns the amount of time until your next birthday
 // This number should be updated in the dom every second and displayed
 // in a timer block on the right side of the screen
-console.log("build some javascript in here")
+
+
+function bDayTimer(){
+  let bday = new Date("October 18, 2019");
+  let now = Date.now();
+  let wait = (bday - now) / 1000;
+  return Math.floor(wait);
+}
+
+setInterval(function(){document.getElementById('timer').innerHTML = bDayTimer();}, 1000);
+console.log(bDayTimer());
